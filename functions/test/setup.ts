c66c853249ff4d899901b6bd4b886ae99b6d7ee0
kosukeoya <kosuke.oya@plaid.co.jp>
2020-09-27 00:49:00 +0900

    fix: config 不要

--- functions/test/setup.ts
+++ functions/test/setup.ts
@@ -1,13 +1,6 @@
 import * as firebaseFunctionsTest from 'firebase-functions-test';
-import * as path from 'path';
 
-const config = {
-  databaseURL: 'https://fir-sandbox-5ed2d.firebaseio.com',
-  storageBucket: 'fir-sandbox-5ed2d.appspot.com',
-  projectId: 'fir-sandbox-5ed2d',
-};
-
-export const test = firebaseFunctionsTest(config, path.join(__dirname, '../../.credentials/serviceAccountKey.json'));
+export const test = firebaseFunctionsTest();
 
 process.on('unhandledRejection', (err) => {
   console.error('UnhandledRejection', err);
