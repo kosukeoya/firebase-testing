1d91f5c326101cf22599069688af887011ec1ab4
kosukeoya <kosuke.oya@plaid.co.jp>
2020-10-04 00:10:22 +0900

    add eslint

--- src/actions.ts
+++ src/actions.ts
@@ -5,5 +5,5 @@ const actionCreator = actionCreatorFactory();
 export const TextInputActions = {
   updateTextInputValue: actionCreator<string>('ACTIONS_UPDATE_TEXT_INPUT_VALUE'),
   updateSelectedValue: actionCreator<string>('ACTION_UPDATE_SELECTED_VALUE'),
-  updateClickCount: actionCreator('ACTION_UPDATE_CLICK_COUNT')
+  updateClickCount: actionCreator('ACTION_UPDATE_CLICK_COUNT'),
 }
